df7187478f381654e14171db146a3bed87b11c1d
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2023-02-10 21:04:02 +0900

    不要なファイルを削除

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+const App = () => {
+  return (
+    <h1>App</h1>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
