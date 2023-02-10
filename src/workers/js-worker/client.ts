2277a842b6117a5ea2c6b6cc1502fdab385ac30b
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2023-02-10 22:32:28 +0900

    classをexportする形に変更

--- src/workers/js-worker/client.ts
+++ src/workers/js-worker/client.ts
@@ -2,7 +2,4 @@ import * as Comlink from "comlink"
 import Worker from './worker?worker'
 import type { Main as WorkerType } from "./worker";
 
-export function main() {
-  const Main = Comlink.wrap<WorkerType>(new Worker());
-  return Main.add(0, 1)
-}
+export const JsWorker = Comlink.wrap<WorkerType>(new Worker())
