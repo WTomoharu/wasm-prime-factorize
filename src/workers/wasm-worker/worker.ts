df435aee5c64254725163cac4bc1db77ea164c90
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2023-02-10 22:32:12 +0900

    awaitのタイミングを変更

--- src/workers/wasm-worker/worker.ts
+++ src/workers/wasm-worker/worker.ts
@@ -1,13 +1,11 @@
 import * as Comlink from "comlink"
 
-let wasm: Promise<WebAssembly.Instance>;
-
-wasm = WebAssembly.instantiateStreaming(fetch('/main.wasm'), {
+const wasm = WebAssembly.instantiateStreaming(fetch('/main.wasm'), {
   env: {  }
 }).then(res => res.instance)
 
 export class Main {
-  async add(a: number, b: number) {
+  async add(a: number, b: number): Promise<number> {
     return (await wasm as any).exports.add(a, b)
   }
 }
