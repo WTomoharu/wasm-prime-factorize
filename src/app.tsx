786a6ca156090e5794ea2c69735e9c9a40ea593c
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2023-02-11 21:29:53 +0900

    appでworkerの呼び出しを実装

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,16 @@
+import { JsWorker } from "./workers/js-worker/client"
+import { WasmWorker } from "./workers/wasm-worker/client"
+
 export const App = () => {
   return (
-    <h1>App</h1>
+    <>
+      <h1>App</h1>
+      <button onClick={() => JsWorker.add(2, 1).then(console.log)}>
+        JS Worker
+      </button>
+      <button onClick={() => WasmWorker.add(2, 1).then(console.log)}>
+        Wasm Worker
+      </button>
+    </>
   )
 }
