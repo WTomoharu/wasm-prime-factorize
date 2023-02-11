b8b88b8a854d101aa3227773e99b836dffc0ec45
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2023-02-12 00:09:43 +0900

    appにuiを追加

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,40 @@
+import { useState } from "react"
 import { JsWorker } from "./workers/js-worker/client"
 import { WasmWorker } from "./workers/wasm-worker/client"
 
 export const App = () => {
+  const [num, setNum] = useState(67447397074609339n)
   return (
     <>
       <h1>App</h1>
-      <button onClick={() => JsWorker.add(2, 1).then(console.log)}>
-        JS Worker
-      </button>
-      <button onClick={() => WasmWorker.add(2, 1).then(console.log)}>
-        Wasm Worker
-      </button>
+      <div>
+        <input
+          value={num.toString()}
+          onChange={e => setNum(BigInt(e.target.value))}
+        />
+      </div>
+      <div>
+        <button onClick={() => {
+          Promise.resolve().then(async () => {
+            const start = performance.now()
+            const res = await JsWorker.primeFactorize(num)
+            const end = performance.now()
+            console.log("js", end - start, res)
+          })
+        }}>
+          JS Worker
+        </button>
+        <button onClick={() => {
+          Promise.resolve().then(async () => {
+            const start = performance.now()
+            const res = await WasmWorker.primeFactorize(num)
+            const end = performance.now()
+            console.log("wasm", end - start, res)
+          })
+        }}>
+          Wasm Worker
+        </button>
+      </div>
     </>
   )
 }
